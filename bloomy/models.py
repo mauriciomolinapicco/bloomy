11da85ceeef10cc7cbf287f2c6bdf7eb5b60d734
unknown <mauriscode@gmail.com>
2024-06-13 15:58:12 -0300

    User management

--- bloomy/models.py
+++ bloomy/models.py
@@ -2,26 +2,31 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    company_name = models.CharField(max_length=255)
-    CNPJ = models.CharField(max_length=14)
-    responsible_person = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    files = models.ManyToManyField('File')
+    company_name = models.CharField(max_length=255, null=True, blank=True)
+    CNPJ = models.CharField(max_length=14, null=True, blank=True)
+    responsible_person = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    userFiles = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
 
 
 class Package(models.Model):
     name = models.CharField(max_length=255)
     allowed_usages = models.IntegerField()
-    price = models. DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
 
 
-class Suscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='suscriptions')
-    package = models.ForeignKey(Package, on_delete=models.SET_NULL)
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscriptions')
+    package = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True)
     start_date = models.DateTimeField(auto_now_add=True)
 
     def getUsesLeft():
@@ -29,10 +34,7 @@ class Suscription(models.Model):
         return 0
     
     def isActive():
-        if getUsesLeft() > 0: 
-            return True
-        else: 
-            return False
+        return self.getUsesLeft() > 0
 
     def __str__(self):
         return f'{self.user.username} - {self.package.name}'
@@ -53,28 +55,26 @@ class Order(models.Model):
     description = models.TextField()
     suggestedText = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    files = models.ManyToManyField('File')    
+    files = models.TextField()    
     status = models.CharField(max_length=100, choices=[
-        ('EM_REVISAO', 'Em revisao'),
-        ('CANCELADO', 'Cancelado'),
+        #('EM_REVISAO', 'Em revisao'),
+        ('A_FAZER', 'A fazer'),
         ('EM_PRODUCAO', 'Em produçao'),
-        ('ENTREGUE', 'Entregue')
+        ('ENTREGUE', 'Entregue'),
+        ('CANCELADO', 'Cancelado'),
     ])
-    specification = models.ForeignKey(Specification, on_delete=models.SET_NULL)
+    specification = models.ForeignKey(Specification, on_delete=models.SET_NULL, null=True)
 
 
     def __str__(self):
         return f'Order {self.id} by {self.user.username}'
 
 
-class File(models.Model):
-    file = models.FileField(upload_to='files/')  
-
-
 class Delivery(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     delivery_date = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
+    
 
     def __str__(self):
             return f'Delivery {self.id} for Order {self.order.id}'
--- bloomy/views.py
+++ bloomy/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users
 
-# Create your views here.
+def client_page(request):
+    return render(request, 'bloomy/cliente.html')
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['fornecedor'])
+def index(request):
+    return render(request, 'bloomy/index.html')
+
+
+@unauthenticated_user
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Usuario ou senha incorreta')
+
+    return render(request, "bloomy/login.html")
+
+
+@unauthenticated_user
+def register(request):
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            messages.success(request, 'A conta foi criada para ' + email)
+            return redirect('login')  # Cambia 'login' por el nombre de tu URL de redirección para el login
+    
+    form = SignUpForm()
+    return render(request, "bloomy/register.html", {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
