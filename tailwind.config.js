9a2da56fe198109897af67004abe8891bb5732ac
mauriciomolinapicco <mauriscode@gmail.com>
2024-08-12 13:52:23 -0300

    Subscriptions page front redoing

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         'rosa': '#DB426F',
         'branco': '#FCFBF7',
       },
+      fontSize: {
+        '7xl': '5rem', // Puedes ajustar el tamaño según tus necesidades
+        '8xl': '6rem', // Puedes ajustar el tamaño según tus necesidades
+      },
     },
   },
   plugins: [],
