40c5d6be80c1103b2644b164f7b78146f2f28b99
mauriciomolinapicco <mauriscode@gmail.com>
2024-08-20 12:12:42 -0300

    settings parameter

--- bloomy_project/settings.py
+++ bloomy_project/settings.py
@@ -172,7 +172,7 @@ AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_VERIFY = True
+AWS_S3_VERIFY_SSL = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
