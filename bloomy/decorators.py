79b5ad11a432d83d67fb36b2be35ebe5e664d439
mauriciomolinapicco <mauriscode@gmail.com>
2024-09-27 13:10:16 -0300

    docstrings for filters and decorators

--- bloomy/decorators.py
+++ bloomy/decorators.py
@@ -3,6 +3,15 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 def unauthenticated_user(view_func):
+    """
+    Decorador para restringir o acesso a usuários autenticados.
+
+    Se o usuário estiver autenticado, redireciona para a página inicial.
+    Caso contrário, permite o acesso à view fornecida.
+
+    Parâmetros:
+        view_func: A função de view que será decorada.
+    """
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('index')
@@ -13,6 +22,16 @@ def unauthenticated_user(view_func):
 
 
 def allowed_users(allowed_roles=[]):
+    """
+    Decorador para permitir o acesso a views somente para usuários de grupos específicos.
+
+    Verifica se o usuário pertence a um dos grupos permitidos. Se sim, permite o acesso à view.
+    Caso contrário, retorna uma mensagem de erro.
+
+    Parâmetros:
+        allowed_roles: Lista de grupos permitidos para acessar a view.
+        view_func: A função de view que será decorada.
+    """
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
 
--- bloomy/filters.py
+++ bloomy/filters.py
@@ -3,6 +3,17 @@ from django_filters import DateFilter, CharFilter
 from .models import Order
 
 class OrderFilter(django_filters.FilterSet):
+    """
+    Classe para filtrar pedidos com base em critérios específicos, como data e nome.
+
+    Atributos:
+        start_date (DateFilter): Filtro para a data de início dos pedidos.
+        end_date (DateFilter): Filtro para a data de término dos pedidos.
+        nome (CharFilter): Filtro para o nome, permitindo uma busca por nome parcial.
+
+    Metaclass:
+        Meta: Especifica o modelo associado e os campos que podem ser filtrados.
+    """
     start_date = DateFilter(field_name="date", lookup_expr='gte', label='Data desde')
     end_date = DateFilter(field_name="date", lookup_expr='lte', label='Data ate')
     nome = CharFilter(field_name='name', lookup_expr='icontains', label='Nome')
